ed5386f745353fb965db80b2a1f3b0bdd2077800
Darrell G Wolfe <darrellgwolfe@outlook.com>
2024-09-01 22:03:10 -0700

    dgw

---
+++ _RefLibraries/z.R_Install.r
@@ -0,0 +1,18 @@
+#The knitr package is not available in this R installation.
+#Install with install.packages("knitr")
+
+#The rmarkdown package is not available in this R installation.
+#Install with install.packages("rmarkdown")
+
+
+install.packages("knitr")
+install.packages("rmarkdown")
+
+#`C:\\Program Files\\R\\R-4.4.1\\bin\\R.exe`
+
+C:\\Users\\darre\\AppData\\Local\\R\\bin\\R.exe
+
+C:\\Users\\darre\\AppData\\Local\\Programs\\R\\R-4.4.1\\bin\\R.exe
+
+
+#C:\\Users\\darre\\AppData\\Local\\Programs\\Quarto\\bin\\quarto.exe
